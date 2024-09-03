b64e4d61ab7daddb4f50117fe067f1e246b026f8
AlejoLg <legname.alejo@gmail.com>
2024-09-03 18:44:33 -0300

    New DB Access class

---
+++ Utils/DataBaseAccess.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Utils
+{
+    public class DataBaseAccess
+    {
+        private SqlConnection connection;
+        private SqlCommand command;
+        private SqlDataReader reader;
+
+        public DataBaseAccess()
+        {
+            connection = new SqlConnection("server=localhost,1434;database=POKEDEX_DB;integrated security=false; user id=sa;password=Alejo4321");
+            command = new SqlCommand();
+
+        }
+
+        public void setQuery(string Query)
+        {
+            command.CommandType = System.Data.CommandType.Text;
+            command.CommandText = Query;
+        }
+
+        public void excecuteQuery()
+        {
+            command.Connection = connection;
+            try
+            {
+                connection.Open();
+                reader = command.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void excecuteAction() //Excecutes actions as update, remove 
+        {
+            command.Connection = connection;
+            try
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void setParameter(string name, object value)
+        {
+            command.Parameters.AddWithValue(name, value);
+        }
+
+        public void CloseConnection()
+        {
+            if (reader != null)
+            {
+                reader.Close();
+            }
+            connection.Close();
+        }
+    }
+}
