f9a317844680236055fac2db0ff5c9f4a30a5464
AlejoLg <legname.alejo@gmail.com>
2024-09-08 23:22:55 -0300

    Form Principal - Images + Add && Modify Article

--- AdministracionArticulos/FormPrincipal.Designer.cs
+++ AdministracionArticulos/FormPrincipal.Designer.cs
@@ -41,9 +41,13 @@
             this.btAgregar = new System.Windows.Forms.Button();
             this.btModificar = new System.Windows.Forms.Button();
             this.btEliminar = new System.Windows.Forms.Button();
+            this.pbPcArticulos = new System.Windows.Forms.PictureBox();
+            this.nudImagenesArticulos = new System.Windows.Forms.NumericUpDown();
             ((System.ComponentModel.ISupportInitialize)(this.dgArticulos)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgMarcas)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgCategorias)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPcArticulos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudImagenesArticulos)).BeginInit();
             this.SuspendLayout();
             // 
             // lbArticulos
@@ -63,7 +67,7 @@
             this.dgArticulos.BackgroundColor = System.Drawing.SystemColors.Window;
             this.dgArticulos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgArticulos.Location = new System.Drawing.Point(13, 80);
-            this.dgArticulos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dgArticulos.Margin = new System.Windows.Forms.Padding(4);
             this.dgArticulos.Name = "dgArticulos";
             this.dgArticulos.ReadOnly = true;
             this.dgArticulos.RowHeadersWidth = 51;
@@ -71,17 +75,20 @@
             this.dgArticulos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dgArticulos.Size = new System.Drawing.Size(859, 177);
             this.dgArticulos.TabIndex = 2;
+            this.dgArticulos.SelectionChanged += new System.EventHandler(this.dgArticulos_SelectionChanged);
             // 
             // dgMarcas
             // 
             this.dgMarcas.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgMarcas.BackgroundColor = System.Drawing.SystemColors.Window;
             this.dgMarcas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgMarcas.Location = new System.Drawing.Point(13, 466);
-            this.dgMarcas.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dgMarcas.Location = new System.Drawing.Point(16, 365);
+            this.dgMarcas.Margin = new System.Windows.Forms.Padding(4);
             this.dgMarcas.Name = "dgMarcas";
+            this.dgMarcas.ReadOnly = true;
             this.dgMarcas.RowHeadersWidth = 51;
-            this.dgMarcas.Size = new System.Drawing.Size(257, 241);
+            this.dgMarcas.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgMarcas.Size = new System.Drawing.Size(257, 239);
             this.dgMarcas.TabIndex = 3;
             // 
             // dgCategorias
@@ -89,18 +96,20 @@
             this.dgCategorias.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgCategorias.BackgroundColor = System.Drawing.SystemColors.Window;
             this.dgCategorias.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgCategorias.Location = new System.Drawing.Point(621, 466);
-            this.dgCategorias.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dgCategorias.Location = new System.Drawing.Point(614, 365);
+            this.dgCategorias.Margin = new System.Windows.Forms.Padding(4);
             this.dgCategorias.Name = "dgCategorias";
+            this.dgCategorias.ReadOnly = true;
             this.dgCategorias.RowHeadersWidth = 51;
-            this.dgCategorias.Size = new System.Drawing.Size(252, 241);
+            this.dgCategorias.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgCategorias.Size = new System.Drawing.Size(257, 241);
             this.dgCategorias.TabIndex = 4;
             // 
             // lbMarcas
             // 
             this.lbMarcas.AutoSize = true;
             this.lbMarcas.Font = new System.Drawing.Font("MS Reference Sans Serif", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbMarcas.Location = new System.Drawing.Point(37, 418);
+            this.lbMarcas.Location = new System.Drawing.Point(79, 315);
             this.lbMarcas.Name = "lbMarcas";
             this.lbMarcas.Size = new System.Drawing.Size(121, 35);
             this.lbMarcas.TabIndex = 5;
@@ -110,7 +119,7 @@
             // 
             this.lbCategorias.AutoSize = true;
             this.lbCategorias.Font = new System.Drawing.Font("MS Reference Sans Serif", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbCategorias.Location = new System.Drawing.Point(672, 418);
+            this.lbCategorias.Location = new System.Drawing.Point(657, 315);
             this.lbCategorias.Name = "lbCategorias";
             this.lbCategorias.Size = new System.Drawing.Size(177, 35);
             this.lbCategorias.TabIndex = 6;
@@ -121,8 +130,8 @@
             this.rdArticulos.AutoSize = true;
             this.rdArticulos.Checked = true;
             this.rdArticulos.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rdArticulos.Location = new System.Drawing.Point(228, 361);
-            this.rdArticulos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rdArticulos.Location = new System.Drawing.Point(197, 742);
+            this.rdArticulos.Margin = new System.Windows.Forms.Padding(4);
             this.rdArticulos.Name = "rdArticulos";
             this.rdArticulos.Size = new System.Drawing.Size(111, 30);
             this.rdArticulos.TabIndex = 7;
@@ -134,21 +143,20 @@
             // 
             this.rdMarcas.AutoSize = true;
             this.rdMarcas.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rdMarcas.Location = new System.Drawing.Point(413, 361);
-            this.rdMarcas.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rdMarcas.Location = new System.Drawing.Point(382, 742);
+            this.rdMarcas.Margin = new System.Windows.Forms.Padding(4);
             this.rdMarcas.Name = "rdMarcas";
             this.rdMarcas.Size = new System.Drawing.Size(93, 30);
             this.rdMarcas.TabIndex = 8;
             this.rdMarcas.Text = "Marca";
             this.rdMarcas.UseVisualStyleBackColor = true;
-            this.rdMarcas.CheckedChanged += new System.EventHandler(this.rdMarcas_CheckedChanged);
             // 
             // rdCategoria
             // 
             this.rdCategoria.AutoSize = true;
             this.rdCategoria.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rdCategoria.Location = new System.Drawing.Point(573, 361);
-            this.rdCategoria.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rdCategoria.Location = new System.Drawing.Point(542, 742);
+            this.rdCategoria.Margin = new System.Windows.Forms.Padding(4);
             this.rdCategoria.Name = "rdCategoria";
             this.rdCategoria.Size = new System.Drawing.Size(130, 30);
             this.rdCategoria.TabIndex = 9;
@@ -159,7 +167,7 @@
             // 
             this.lbOpcionElemento.AutoSize = true;
             this.lbOpcionElemento.Font = new System.Drawing.Font("MS Reference Sans Serif", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbOpcionElemento.Location = new System.Drawing.Point(272, 297);
+            this.lbOpcionElemento.Location = new System.Drawing.Point(275, 684);
             this.lbOpcionElemento.Name = "lbOpcionElemento";
             this.lbOpcionElemento.Size = new System.Drawing.Size(339, 35);
             this.lbOpcionElemento.TabIndex = 10;
@@ -167,10 +175,10 @@
             // 
             // btAgregar
             // 
-            this.btAgregar.Location = new System.Drawing.Point(316, 442);
-            this.btAgregar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btAgregar.Location = new System.Drawing.Point(245, 793);
+            this.btAgregar.Margin = new System.Windows.Forms.Padding(4);
             this.btAgregar.Name = "btAgregar";
-            this.btAgregar.Size = new System.Drawing.Size(295, 60);
+            this.btAgregar.Size = new System.Drawing.Size(395, 60);
             this.btAgregar.TabIndex = 11;
             this.btAgregar.Text = "Agregar";
             this.btAgregar.UseVisualStyleBackColor = true;
@@ -178,10 +186,10 @@
             // 
             // btModificar
             // 
-            this.btModificar.Location = new System.Drawing.Point(316, 527);
-            this.btModificar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btModificar.Location = new System.Drawing.Point(245, 878);
+            this.btModificar.Margin = new System.Windows.Forms.Padding(4);
             this.btModificar.Name = "btModificar";
-            this.btModificar.Size = new System.Drawing.Size(295, 60);
+            this.btModificar.Size = new System.Drawing.Size(395, 60);
             this.btModificar.TabIndex = 12;
             this.btModificar.Text = "Modificar";
             this.btModificar.UseVisualStyleBackColor = true;
@@ -189,21 +197,41 @@
             // 
             // btEliminar
             // 
-            this.btEliminar.Location = new System.Drawing.Point(316, 610);
-            this.btEliminar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btEliminar.Location = new System.Drawing.Point(245, 961);
+            this.btEliminar.Margin = new System.Windows.Forms.Padding(4);
             this.btEliminar.Name = "btEliminar";
-            this.btEliminar.Size = new System.Drawing.Size(295, 60);
+            this.btEliminar.Size = new System.Drawing.Size(395, 60);
             this.btEliminar.TabIndex = 13;
             this.btEliminar.Text = "Eliminar";
             this.btEliminar.UseVisualStyleBackColor = true;
             this.btEliminar.Click += new System.EventHandler(this.btEliminar_Click);
+            // 
+            // pbPcArticulos
+            // 
+            this.pbPcArticulos.Location = new System.Drawing.Point(306, 365);
+            this.pbPcArticulos.Name = "pbPcArticulos";
+            this.pbPcArticulos.Size = new System.Drawing.Size(272, 241);
+            this.pbPcArticulos.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbPcArticulos.TabIndex = 14;
+            this.pbPcArticulos.TabStop = false;
+            // 
+            // nudImagenesArticulos
+            // 
+            this.nudImagenesArticulos.Location = new System.Drawing.Point(382, 612);
+            this.nudImagenesArticulos.Name = "nudImagenesArticulos";
+            this.nudImagenesArticulos.ReadOnly = true;
+            this.nudImagenesArticulos.Size = new System.Drawing.Size(120, 22);
+            this.nudImagenesArticulos.TabIndex = 15;
+            this.nudImagenesArticulos.ValueChanged += new System.EventHandler(this.nudImagenesArticulos_ValueChanged);
             // 
             // formPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.AntiqueWhite;
-            this.ClientSize = new System.Drawing.Size(889, 722);
+            this.ClientSize = new System.Drawing.Size(885, 1036);
+            this.Controls.Add(this.nudImagenesArticulos);
+            this.Controls.Add(this.pbPcArticulos);
             this.Controls.Add(this.btEliminar);
             this.Controls.Add(this.btModificar);
             this.Controls.Add(this.btAgregar);
@@ -226,6 +254,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.dgArticulos)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgMarcas)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgCategorias)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPcArticulos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudImagenesArticulos)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -245,6 +275,8 @@
         private System.Windows.Forms.Button btAgregar;
         private System.Windows.Forms.Button btModificar;
         private System.Windows.Forms.Button btEliminar;
+        private System.Windows.Forms.PictureBox pbPcArticulos;
+        private System.Windows.Forms.NumericUpDown nudImagenesArticulos;
     }
 }
 
--- AdministracionArticulos/FormPrincipal.cs
+++ AdministracionArticulos/FormPrincipal.cs
@@ -17,6 +17,7 @@ namespace AdministracionArticulos
         private List<Articulo> listArticulos;
         private List<Categoria> listCategorias;
         private List<Marca> listMarca;
+        private List<string> listImagenes;
         private bool Marca;
 
         public formPrincipal()
@@ -30,25 +31,108 @@ namespace AdministracionArticulos
             Marca = false; 
         }
 
+        /*---------------- EVENTS ----------------*/
+
         private void btAgregar_Click(object sender, EventArgs e)
         {
-           
+           if (rdArticulos.Checked)
+            {
+                FormArticulo articulo = new FormArticulo();
+                articulo.ShowDialog();
+            }
+           else if (rdMarcas.Checked)
+            {
+                //Agregar Marca
+            }
+            else
+            {
+                //Agregar Categoría
+            }
+            cargarGrids();
         }
 
         private void btModificar_Click(object sender, EventArgs e)
         {
-          
+            if (rdArticulos.Checked)
+            {
+                Articulo selectedArt;
+                selectedArt = (Articulo)dgArticulos.CurrentRow.DataBoundItem;
+                FormArticulo articulo = new FormArticulo(selectedArt);
+                articulo.ShowDialog();
+            }
+            else if (rdMarcas.Checked)
+            {
+                //Modificar Marca
+            }
+            else
+            {
+                //Modificar Categoría
+            }
+            cargarGrids();
         }
 
         private void btEliminar_Click(object sender, EventArgs e)
         {
-           
+            if (rdArticulos.Checked)
+            {
+                deleteArticulo();
+            }
+            else if (rdMarcas.Checked)
+            {
+                //Eliminar Marca
+            }
+            else
+            {
+                //Eliminar Categoría
+            }
+            cargarGrids();
         }
 
-        private void rdMarcas_CheckedChanged(object sender, EventArgs e)
+        private void dgArticulos_SelectionChanged(object sender, EventArgs e)
         {
-         
+            try
+            {
+                ArticuloServices articuloService = new ArticuloServices();
+                Articulo articuloSeleccionado = (Articulo)dgArticulos.CurrentRow.DataBoundItem;
+                listImagenes = articuloService.listarImagenes(articuloSeleccionado.Codigo);
+                nudImagenesArticulos.Value = 0;
+                nudImagenesArticulos.Maximum = listImagenes.Count;
+                if (listImagenes.Count > 0)
+                {
+                    uploadImage(listImagenes[(int)nudImagenesArticulos.Value]);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
+
+        private void nudImagenesArticulos_ValueChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                ArticuloServices articuloService = new ArticuloServices();
+                Articulo articuloSeleccionado = (Articulo)dgArticulos.CurrentRow.DataBoundItem;
+                listImagenes = articuloService.listarImagenes(articuloSeleccionado.Codigo);
+                if (listImagenes.Count > 0)
+                {
+                    nudImagenesArticulos.Maximum = listImagenes.Count - 1;
+                    uploadImage(listImagenes[(int)nudImagenesArticulos.Value]);
+                }
+                else
+                {
+                    uploadImage("DefaultImage");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        /*---------------- FUNCTIONS ----------------*/
 
         private void cargarGrids()
         {
@@ -59,18 +143,33 @@ namespace AdministracionArticulos
             try
             {
                 listArticulos = articuloService.listar();
+                if (listArticulos.Count > 0)
+                {
+                listImagenes = articuloService.listarImagenes(listArticulos[0].Codigo);
+                }
+
                 dgArticulos.DataSource = listArticulos;
                 dgArticulos.Columns["Id"].Visible = false;
                 dgArticulos.Columns["Imagen"].Visible = false;
-                AjustarDgArticulosView(dgArticulos);
+
+                if (listImagenes.Count > 0)
+                {
+                    uploadImage(listImagenes[0]);
+                }
+                else
+                {
+                    uploadImage("DefaultImage");
+                }
+
+                AjustarDgView(dgArticulos, 6);
 
                 listCategorias = categoriaService.listar();
                 dgCategorias.DataSource = listCategorias;
-                AjustarDgArticulosView(dgCategorias);
+                AjustarDgView(dgCategorias, 6);
 
                 listMarca = marcaService.listar();
                 dgMarcas.DataSource = listMarca;
-                AjustarDgArticulosView(dgMarcas);
+                AjustarDgView(dgMarcas, 6);
 
             }
             catch (Exception ex)
@@ -79,10 +178,9 @@ namespace AdministracionArticulos
             }
         }
 
-        private void AjustarDgArticulosView(DataGridView dataGridView)
+        private void AjustarDgView(DataGridView dataGridView, int maxVisibleRows)
         {
             int recordCount = dataGridView.Rows.Count;
-            int maxVisibleRows = 6;
             int rowHeight = dataGridView.RowTemplate.Height;
             int headerHeight = dataGridView.ColumnHeadersHeight;
 
@@ -101,6 +199,39 @@ namespace AdministracionArticulos
             }
         }
 
-        
+        private void uploadImage(string image)
+        {
+            try
+            {
+                pbPcArticulos.Load(image);
+            }
+            catch (Exception ex)
+            {
+                pbPcArticulos.Load("https://static.vecteezy.com/system/resources/previews/004/141/669/non_2x/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg");
+            }
+        }
+
+        public void deleteArticulo()
+        {
+            ArticuloServices service = new ArticuloServices();
+            Articulo selectedArt;
+
+            try
+            {
+                selectedArt = (Articulo)dgArticulos.CurrentRow.DataBoundItem;
+                DialogResult response = MessageBox.Show($"¿Éstá seguro de eliminar el Artículo '{selectedArt.Nombre}' con código '{selectedArt.Codigo}'?", "Eliminar Artículo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (response == DialogResult.Yes)
+                {
+                    service.delete(selectedArt.Id);
+                    cargarGrids();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
     }
 }
